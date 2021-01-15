7732579c989b1152c0e3009211d5f0735d4a642c
Bruno <bruno.vallet@ign.fr>
2021-01-15 12:59:00 +0100

    Trying to fix people.js

--- js/people.js
+++ js/people.js
@@ -45,7 +45,7 @@
 								if (d2.getTime() < d1.getTime()) {
 									var parent = document.getElementById("alumni-container");
 								} else {
-									if(row.data[0].statut == 'Doctorant' || row.data[0].statut == 'Doctorante' || row.data[0].statut == 'Post-doc'){
+									if(row.data[0].status == 'PhD student' || row.data[0].status == 'Post-doc'){
 										var parent = document.getElementById("non-permanent-container");
 									}
 								}
